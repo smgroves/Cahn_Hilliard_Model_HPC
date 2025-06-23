70915f3f46f458683160fd556dd6e7fb6d2c45ae
Sarah Groves <sarahmaddoxgroves@gmail.com>
2025-06-23 12:02:24 -0400

    IC for spinodal decomp Figure 2

--- spinodal_decomp/run_julia.sh
+++ spinodal_decomp/run_julia.sh
@@ -6,13 +6,13 @@
 #SBATCH --time=8:00:00
 #SBATCH --mem=100G
 #SBATCH --partition=standard
-#SBATCH --array=1
+#SBATCH --array=1-96
 
 echo $(date)
-OPTS=$(sed -n "${SLURM_ARRAY_TASK_ID}"p opts_redo_Julia_v2.txt)
+OPTS=$(sed -n "${SLURM_ARRAY_TASK_ID}"p opts_Julia.txt)
 echo $OPTS
 
-outdir="/project/g_bme-janeslab/SarahG/spinodal_decomp_04_2025/out_julia"
+outdir="/project/g_bme-janeslab/SarahG/spinodal_decomp_06_2025/out_julia"
 mkdir -p $outdir
 # Load  Julia environment
 module load julia/1.9.2
@@ -22,7 +22,7 @@ SLURM_ID=${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}
 
 # if [[ "$solver" == "SAV"  ]]; then
 # julia ./CahnHilliard_Julia_solvers/run_spinodal_decomp.jl $OPTS "_25p" $SLURM_ID 
-julia ./CahnHilliard_Julia_solvers/run_spinodal_decomp.jl $OPTS $SLURM_ID 
+julia ./CahnHilliard_Julia_solvers/run_spinodal_decomp_HPC.jl $OPTS $SLURM_ID 
 
 # fi
 
